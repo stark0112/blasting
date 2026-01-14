d7f150ece61e64c42660537104a256a5d1fa0bd0
teramime-dev <teramime.dev@gmail.com>
2026-01-15 00:05:11 +0900

    표 패딩 7px -> 8.5px

--- streamlit_app.py
+++ streamlit_app.py
@@ -400,7 +400,7 @@ if "result" in st.session_state:
         <style>
         [data-testid="stMarkdownContainer"] table td,
         [data-testid="stMarkdownContainer"] table th {
-            padding: 7px 10px !important;
+            padding: 8.5px 10px !important;
         }
         </style>
         """, unsafe_allow_html=True)
